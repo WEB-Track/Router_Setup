c6ced4754e173d85ee709f2ea0745363acf194ee
William Bullock <63573688+WEB-Track@users.noreply.github.com>
2024-09-19 15:36:45 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script>
+import TheNavigation from '@/components/TheNavigation.vue';
+export default{
+  components: {TheNavigation},
+}
+</script>
+
+<template>
+  <div class="app">
+    <TheNavigation/>
+  </div>
+  <div class="container">
+    <RouterView v-slot="{Component}">
+      <transition name="moveUp">
+        <component :is="Component" :key="$router.path"></component>
+      </transition>
+    </RouterView>
+  </div>
+</template>
+
+  <style lang="css">
+    .moveUp-enter-active{
+      animation: fadeIn 1s ease-in;
+    }  
+    @keyframes fadeIn {
+      0% { opacity: 0; }
+      50% { opacity: 0.5; }
+      100% { opacity: 1; }
+    }
+    .moveUp-leave-active {
+      animation: moveUp 0.4s ease-in;
+    }
+    @keyframes moveUp {
+      0% {transform: translateY(0);}
+      100% {transform: translateY(-400px);}
+    }
+  </style>
---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,30 @@
+<script>
+  import sourceData from '@/data.json'
+  export default {
+    data(){
+      return {
+        destinations: sourceData.destinations
+      }
+    }
+  }
+</script>
+
+<template>
+  <div id="nav">
+    <router-link id="logo" to="/">Vue School Travel App</router-link>
+   <router-link 
+   v-for="destination in destinations"
+   :key="destination.id"
+   :to="{name: 'destination.show', params:{id: destination.id, slug: destination.slug}}"
+   >
+    {{ destination.name }}
+   </router-link>
+   </div>
+</template>
+
+<style>
+ #nav .vue-school-active-link{
+  color: rgb(254, 125, 125);
+  border-bottom: 2px solid rgb(254, 125, 125);
+ }
+</style>
